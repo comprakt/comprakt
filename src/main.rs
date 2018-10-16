3ce138c2cde4eaedd5f0602979c43177ee4a8130
flip1995 <hello@philkrones.com>
2018-10-16 17:11:43 +0200

    Run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,24 @@ use structopt::StructOpt;
 use std::path::PathBuf;
 
 #[derive(StructOpt)]
-#[structopt(name="comprakt")]
+#[structopt(name = "comprakt")]
 struct Opt {
-    #[structopt(long="echo")]
+    #[structopt(long = "echo")]
     echo: bool,
-    #[structopt(name="FILE", parse(from_os_str))]
+    #[structopt(name = "FILE", parse(from_os_str))]
     file: PathBuf,
 }
 
-use std::io;
-use std::fs::File;
+use std::{fs::File, io};
 
 fn main() {
     let opt = Opt::from_args();
 
     if opt.echo {
-        let mut f = File::open(&opt.file).unwrap_or_else(|e| panic!("could not open file {:?}: {}", &opt.file, e));
+        let mut f = File::open(&opt.file)
+            .unwrap_or_else(|e| panic!("could not open file {:?}: {}", &opt.file, e));
         let mut stdout = io::stdout();
         io::copy(&mut f, &mut stdout).expect("could not read from input or write to stdout");
         return;
     }
-
 }
