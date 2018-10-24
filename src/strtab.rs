92f3a7565d27d6d39835821e5923195321a2a18e
Joshua Bachmeier <joshua@bachmeier.cc>
2018-10-24 21:33:47 +0200

    Add strtab tests

--- src/strtab.rs
+++ src/strtab.rs
@@ -31,3 +31,69 @@ impl StringTable {
         Rc::clone(entries.get(value).unwrap())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    //! Tests ideas are stolen to a not insignificant degree from Reiner
+
+    use super::*;
+    use std::collections::HashMap;
+
+    macro_rules! get {
+        ($tab: expr) => {
+            unsafe { &mut *$tab.entries.get() }
+        };
+    }
+
+    #[test]
+    fn no_duplication() {
+        let strtab = StringTable::new();
+
+        strtab.intern("foo");
+        strtab.intern("foo");
+        strtab.intern("foo");
+        assert_eq!(1, get!(strtab).len());
+
+        strtab.intern("bar");
+        strtab.intern("bar");
+        strtab.intern("foo");
+        assert_eq!(2, get!(strtab).len());
+    }
+
+    #[test]
+    fn can_resize_set() {
+        let strtab = StringTable::new();
+        get!(strtab).shrink_to_fit();
+
+        let n = 100_000;
+        let mut adresses = HashMap::new();
+
+        for i in 0..n {
+            let s = format!("s{}", i);
+            let sym = Rc::into_raw(strtab.intern(&s));
+            adresses.insert(s, sym);
+        }
+
+        assert_eq!(n, get!(strtab).len());
+        // At this point, the table probably got resized and reallocated, so let's now
+        // check if all the symbols are still in the same place
+
+        for i in 0..n {
+            let s = format!("s{}", i);
+            assert_eq!(
+                adresses.remove(&s).unwrap() as *const u8 as usize,
+                Rc::into_raw(strtab.intern(&s)) as *const u8 as usize
+            );
+        }
+    }
+
+    #[test]
+    fn can_intern_empty_string() {
+        let strtab = StringTable::new();
+
+        strtab.intern("");
+        strtab.intern("");
+        strtab.intern("");
+        assert_eq!(1, get!(strtab).len());
+    }
+}
