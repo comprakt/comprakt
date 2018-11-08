21e9a3a9d83602d4dfcf36001912351a872403fa
Joshua Bachmeier <joshua@bachmeier.cc>
2018-11-08 21:57:23 +0100

    Make Spanned PartialEq, Eq and Deref to data

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,7 +5,7 @@ use crate::{
     strtab::*,
 };
 use failure::Fail;
-use std::{convert::TryFrom, fmt, result::Result};
+use std::{convert::TryFrom, fmt, ops::Deref, result::Result};
 
 macro_rules! match_op {
     ($input:expr, $( ($token_string:expr, $token:expr) ),+: $len:expr, $default:expr) => {{
@@ -36,6 +36,27 @@ pub type LexicalError<'f> = Spanned<'f, ErrorKind>;
 pub struct Spanned<'f, T> {
     pub span: Span<'f>,
     pub data: T,
+}
+
+impl<'f, T> Eq for Spanned<'f, T> where T: Eq {}
+impl<'f, T> PartialEq for Spanned<'f, T>
+where
+    T: PartialEq,
+{
+    /// This only compares the `data`! I.e. two `Spanned`s are equal even if
+    /// they point to two different spans in the source file, as long as the
+    /// content is the same.
+    fn eq(&self, other: &Self) -> bool {
+        self.data == other.data
+    }
+}
+
+impl<'f, T> Deref for Spanned<'f, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
 }
 
 impl<T> fmt::Display for Spanned<'_, T>
