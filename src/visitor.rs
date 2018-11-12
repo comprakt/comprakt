0e2c6493da044bcc5b3bc8bb51e57494b95875a1
flip1995 <hello@philkrones.com>
2018-11-12 17:35:59 +0100

    Make most of the visitor NodeKinds Spanned

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,20 +1,21 @@
 use crate::ast::{self, *};
 use strum_macros::EnumDiscriminants;
+use asciifile::Spanned;
 
 #[strum_discriminants(derive(Display))]
 #[derive(EnumDiscriminants)]
 pub enum NodeKind<'a, 't> {
     AST(&'a AST<'t>),
-    Program(&'a Program<'t>),
-    ClassDeclaration(&'a ClassDeclaration<'t>),
-    ClassMember(&'a ClassMember<'t>),
-    Parameter(&'a Parameter<'t>),
-    ParameterList(&'a ParameterList<'t>),
-    Type(&'a Type),
+    Program(&'a Spanned<'t, Program<'t>>),
+    ClassDeclaration(&'a Spanned<'t, ClassDeclaration<'t>>),
+    ClassMember(&'a Spanned<'t, ClassMember<'t>>),
+    Parameter(&'a Spanned<'t, Parameter<'t>>),
+    ParameterList(&'a Spanned<'t, ParameterList<'t>>),
+    Type(&'a Spanned<'t, Type>),
     BasicType(&'a BasicType),
-    Block(&'a Block<'t>),
-    Stmt(&'a Stmt<'t>),
-    Expr(&'a Expr<'t>),
+    Block(&'a Spanned<'t, Block<'t>>),
+    Stmt(&'a Spanned<'t, Stmt<'t>>),
+    Expr(&'a Spanned<'t, Expr<'t>>),
     BinaryOp(&'a BinaryOp),
     UnaryOp(&'a UnaryOp),
 }
@@ -35,6 +36,7 @@ macro_rules! gen_nodekind_match {
             NodeKind::Expr($varname) => $rhs,
             NodeKind::BinaryOp($varname) => $rhs,
             NodeKind::UnaryOp($varname) => $rhs,
+            NodeKind::PostfixOp($varname) => $rhs,
         }
     };
 }
@@ -51,7 +53,7 @@ impl<'a, 't> NodeKind<'a, 't> {
         use self::NodeKind::*;
         macro_rules! ccb {
             ($astvar:expr) => {
-                cb(NodeKind::from(&$astvar.data))
+                cb(NodeKind::from($astvar))
             };
         }
         match self {
@@ -70,7 +72,7 @@ impl<'a, 't> NodeKind<'a, 't> {
                     Field(t) => ccb!(t),
                     Method(ty, pl, block) => {
                         ccb!(ty);
-                        cb(NodeKind::ParameterList(&pl.data));
+                        cb(NodeKind::ParameterList(&pl));
                         ccb!(block);
                     }
                     MainMethod(_, block) => {
@@ -87,7 +89,7 @@ impl<'a, 't> NodeKind<'a, 't> {
             Block(b) => b.statements.iter().for_each(|x| ccb!(x)),
             Stmt(s) => {
                 use crate::ast::Stmt::*;
-                match s {
+                match &s.data {
                     Empty => (),
                     Block(b) => ccb!(b),
                     Expression(e) => ccb!(e.as_ref()),
@@ -109,7 +111,7 @@ impl<'a, 't> NodeKind<'a, 't> {
             }
             Expr(e) => {
                 use crate::ast::Expr::*;
-                match e {
+                match &e.data {
                     Binary(_, lhs, rhs) => {
                         ccb!(lhs.as_ref());
                         ccb!(rhs.as_ref());
@@ -136,9 +138,9 @@ impl<'a, 't> NodeKind<'a, 't> {
 }
 
 macro_rules! gen_from_ast {
-    ($nodekind:path, $asttype:ty) => {
-        impl<'a, 'f> From<&'a $asttype> for NodeKind<'a, 'f> {
-            fn from(a: &'a $asttype) -> Self {
+    ($nodekind:path, $spanned_asttype:ty) => {
+        impl<'a, 'f> From<&'a $spanned_asttype> for NodeKind<'a, 'f> {
+            fn from(a: &'a $spanned_asttype) -> Self {
                 $nodekind(a)
             }
         }
@@ -146,18 +148,18 @@ macro_rules! gen_from_ast {
 }
 
 gen_from_ast!(NodeKind::AST<'a, 'f>, ast::AST<'f>);
-gen_from_ast!(NodeKind::Program<'a, 'f>, ast::Program<'f>);
+gen_from_ast!(NodeKind::Program<'a, 'f>, Spanned<'f, ast::Program<'f>>);
 gen_from_ast!(
     NodeKind::ClassDeclaration<'a, 'f>,
-    ast::ClassDeclaration<'f>
+    Spanned<'f, ast::ClassDeclaration<'f>>
 );
-gen_from_ast!(NodeKind::ClassMember<'a, 'f>, ast::ClassMember<'f>);
-gen_from_ast!(NodeKind::Parameter<'a, 'f>, ast::Parameter<'f>);
-gen_from_ast!(NodeKind::ParameterList<'a, 'f>, ast::ParameterList<'f>);
-gen_from_ast!(NodeKind::Type<'a, 'f>, ast::Type);
+gen_from_ast!(NodeKind::ClassMember<'a, 'f>, Spanned<'f, ast::ClassMember<'f>>);
+gen_from_ast!(NodeKind::Parameter<'a, 'f>, Spanned<'f, ast::Parameter<'f>>);
+gen_from_ast!(NodeKind::ParameterList<'a, 'f>, Spanned<'f, ast::ParameterList<'f>>);
+gen_from_ast!(NodeKind::Type<'a, 'f>, Spanned<'f, ast::Type>);
 gen_from_ast!(NodeKind::BasicType<'a, 'f>, ast::BasicType);
-gen_from_ast!(NodeKind::Block<'a, 'f>, ast::Block<'f>);
-gen_from_ast!(NodeKind::Stmt<'a, 'f>, ast::Stmt<'f>);
-gen_from_ast!(NodeKind::Expr<'a, 'f>, ast::Expr<'f>);
+gen_from_ast!(NodeKind::Block<'a, 'f>, Spanned<'f, ast::Block<'f>>);
+gen_from_ast!(NodeKind::Stmt<'a, 'f>, Spanned<'f, ast::Stmt<'f>>);
+gen_from_ast!(NodeKind::Expr<'a, 'f>, Spanned<'f, ast::Expr<'f>>);
 gen_from_ast!(NodeKind::BinaryOp<'a, 'f>, ast::BinaryOp);
 gen_from_ast!(NodeKind::UnaryOp<'a, 'f>, ast::UnaryOp);
