4f51caf66b80586c67f88295767de97b2c9e8d53
Christian Schwarz <me@cschwarz.com>
2018-11-20 16:54:55 +0100

    libfirm-rs-bindings: rewrite rules for some enums

--- libfirm-rs-bindings/build.rs
+++ libfirm-rs-bindings/build.rs
@@ -18,6 +18,98 @@ fn cargo_envpath(varname: &str) -> PathBuf {
 
 use std::fs;
 
+#[derive(Debug, Clone, Copy)]
+enum ConstifiedEnumRewrite {
+    /// enum_name is something like `Some("enum ir_relation")`
+    /// variant is something like `ir_relation_.*`
+    WithEnumPrefix(&'static str, Option<&'static str>),
+    /// enum_name is something like `Some("ir_relation")`
+    /// variant is something like `ir_relation_.*`
+    Solo(&'static str),
+}
+
+impl ConstifiedEnumRewrite {
+    fn bindgen_module_name(&self) -> String {
+        match self {
+            ConstifiedEnumRewrite::WithEnumPrefix(n, _) => format!("{}", n),
+            ConstifiedEnumRewrite::Solo(n) => format!("{}", n),
+        }
+    }
+    fn bindgen_enum_name(&self) -> String {
+        match self {
+            ConstifiedEnumRewrite::WithEnumPrefix(n, _) => format!("enum {}", n),
+            ConstifiedEnumRewrite::Solo(n) => format!("{}", n),
+        }
+    }
+    fn trim_start_matches_prefix(&self) -> String {
+        match self {
+            ConstifiedEnumRewrite::WithEnumPrefix(_, Some(strip_prefix)) => {
+                format!("{}", strip_prefix)
+            }
+            ConstifiedEnumRewrite::WithEnumPrefix(n, None) => format!("{}_", n),
+            ConstifiedEnumRewrite::Solo(n) => format!("{}_", n),
+        }
+    }
+}
+
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+struct EnumRewriter {
+    rules: Vec<ConstifiedEnumRewrite>,
+    rewrites: HashMap<String, String>, // bindgen_enum_name => trim_start_matches prefix
+}
+
+impl EnumRewriter {
+    fn from_rewrites(rules: Vec<ConstifiedEnumRewrite>) -> Self {
+        let mut rewrites = HashMap::new();
+        for rule in &rules {
+            let res = rewrites.insert(rule.bindgen_enum_name(), rule.trim_start_matches_prefix());
+            debug_assert!(
+                res.is_none(),
+                "duplicate in constified_enum_rewrites: {:?}",
+                rule
+            );
+        }
+        return EnumRewriter { rewrites, rules };
+    }
+    fn builder_with_rewrites(&self, builder: bindgen::Builder) -> bindgen::Builder {
+        let mut builder = builder;
+        for rule in &self.rules {
+            builder = builder.constified_enum_module(rule.bindgen_module_name());
+        }
+        return builder;
+    }
+}
+
+use bindgen::callbacks::*;
+use inflections::case;
+
+impl bindgen::callbacks::ParseCallbacks for EnumRewriter {
+    fn enum_variant_name(
+        &self,
+        enum_name: Option<&str>,
+        original_variant_name: &str,
+        variant_value: EnumVariantValue,
+    ) -> Option<String> {
+        if let Some(trim_prefix) = enum_name.and_then(|n| self.rewrites.get(n)) {
+            let trimmed = original_variant_name.trim_start_matches(trim_prefix);
+            let rewritten = case::to_pascal_case(trimmed);
+            println!(
+                "rewriting {:?} {:?} => {:?}",
+                rewritten, original_variant_name, rewritten,
+            );
+            return Some(rewritten);
+        }
+
+        println!(
+            "unhandled: {:?} {:?} {:?}",
+            enum_name, original_variant_name, variant_value
+        );
+        return None;
+    }
+}
+
 fn main() {
     let libfirm_src = cargo_envpath("CARGO_MANIFEST_DIR").join("libfirm");
     let libfirm_src = fs::canonicalize(&libfirm_src).expect(&format!(
@@ -59,16 +151,28 @@ fn main() {
     tell_cargo!("rustc-link-search", libfirm_lib_dir.display());
     tell_cargo!("rustc-link-lib=static", "firm");
 
-    let bindings = bindgen::Builder::default()
+    let builder = bindgen::Builder::default()
         .header("wrapper.h")
         .clang_args(&[&format!("-I{}", libfirm_include_dir.display())])
         // bindgen's own tests fail on that, see
         // https://github.com/rust-lang-nursery/rust-bindgen/issues/550#issuecomment-283438998
         .blacklist_type("max_align_t")
         // comments include code examples which don't work without using self
-        .generate_comments(false)
-        .generate()
-        .expect("Failed to generate bindings");
+        .generate_comments(false);
+
+    let enum_rewriter = EnumRewriter::from_rewrites(vec![
+        ConstifiedEnumRewrite::WithEnumPrefix("mtp_additional_properties", Some("mtp_")),
+        ConstifiedEnumRewrite::WithEnumPrefix("ir_relation", None),
+        ConstifiedEnumRewrite::Solo("pn_Cond"),
+        ConstifiedEnumRewrite::Solo("pn_Load"),
+        ConstifiedEnumRewrite::Solo("pn_Store"),
+        ConstifiedEnumRewrite::WithEnumPrefix("ir_cons_flags", Some("cons_")),
+    ]);
+
+    let builder = builder.parse_callbacks(Box::new(enum_rewriter.clone()));
+    let builder = enum_rewriter.builder_with_rewrites(builder);
+
+    let bindings = builder.generate().expect("Failed to generate bindings");
 
     let out_path = cargo_envpath("OUT_DIR").join("bindings.rs");
     bindings
