7705e65a3403d8ecc24a8fb0308255a81c618cdb
Christian Schwarz <me@cschwarz.com>
2018-11-21 12:27:22 +0100

    libfirm-rs: abstractions for primitives and array types

--- libfirm-rs/src/lib.rs
+++ libfirm-rs/src/lib.rs
@@ -1,6 +1,6 @@
 pub use libfirm_rs_bindings as bindings;
 
-use libfirm_rs_bindings::ir_type;
+use libfirm_rs_bindings::*;
 
 #[derive(Clone, Copy)]
 pub struct Ty(*mut ir_type);
@@ -14,6 +14,36 @@ impl Into<*mut ir_type> for Ty {
 impl From<*mut ir_type> for Ty {
     fn from(primitive: *mut ir_type) -> Ty {
         Ty(primitive)
+    }
+}
+
+impl Ty {
+    pub fn pointer(self) -> Ty {
+        unsafe { new_type_pointer(self.into()) }.into()
+    }
+}
+
+pub struct PrimitiveType;
+impl PrimitiveType {
+    pub fn from_mode(mode: *mut libfirm_rs_bindings::ir_mode) -> Ty {
+        unsafe { new_type_primitive(mode) }.into()
+    }
+    pub fn isize() -> Ty {
+        unsafe { new_type_primitive(mode::Is) }.into()
+    }
+    pub fn usize() -> Ty {
+        unsafe { new_type_primitive(mode::Iu) }.into()
+    }
+}
+
+pub struct ArrayType;
+
+impl ArrayType {
+    pub fn varlength(element_type: Ty) -> Ty {
+        unsafe { new_type_array(element_type.into(), 0) }.into()
+    }
+    pub fn fixedlength(element_type: Ty, length: usize) -> Ty {
+        unsafe { new_type_array(element_type.into(), length as u32) }.into()
     }
 }
 
