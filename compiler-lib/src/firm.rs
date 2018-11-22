fd89cd8cdaddb33c0841e45b4d649436b51d6622
Joshua Bachmeier <joshua@bachmeier.cc>
2018-11-23 00:59:29 +0100

    ir_gen: Implement control flow gen (except while)
    Expressions also only stubs

--- compiler-lib/src/firm.rs
+++ compiler-lib/src/firm.rs
@@ -1,10 +1,13 @@
 use crate::{
+    asciifile::Spanned,
     ast::{self, Program, AST},
-    type_checking::type_system::{CheckedType, TypeSystem},
+    strtab::Symbol,
+    type_checking::type_system::{CheckedType, ClassMethodDef, TypeSystem},
     visitor::NodeKind,
 };
 use libfirm_rs::{bindings::*, *};
 use std::{
+    collections::HashMap,
     ffi::{CStr, CString, OsStr},
     path::PathBuf,
 };
@@ -71,18 +74,13 @@ impl<'ir, 'src> FirmGenerator<'ir, 'src> {
                     let graph = Graph::function(
                         &method_name,
                         method_type,
-                        method.params.len() + local_var_def_visitor.count,
+                        // "+1" is THIS-ptr
+                        method.params.len() + 1 + local_var_def_visitor.count,
                     );
-                    let start_block = graph.start_block();
-                    // FIXME: Swap this with body generation
-                    let mem = graph.cur_store();
-                    let ret = start_block.new_return(mem, None);
-                    let end_block = graph.end_block();
 
-                    end_block.add_pred(&ret);
+                    MethodBodyGenerator::new(graph, method).gen_method(&block);
 
                     unsafe {
-                        mature_immBlock(get_irg_start_block(graph.into()));
                         irg_finalize_cons(graph.into());
                     }
                     fn_graphs.push(graph);
@@ -91,6 +89,205 @@ impl<'ir, 'src> FirmGenerator<'ir, 'src> {
         }
 
         fn_graphs
+    }
+}
+
+struct MethodBodyGenerator<'ir, 'src> {
+    graph: Graph,
+    method_def: &'ir ClassMethodDef<'src>,
+    local_vars: HashMap<Symbol<'src>, (usize, mode::Type)>,
+    num_vars: usize,
+}
+
+impl<'a, 'ir, 'src> MethodBodyGenerator<'ir, 'src> {
+    fn new(graph: Graph, method_def: &'ir ClassMethodDef<'src>) -> Self {
+        let mut se1f = MethodBodyGenerator {
+            graph,
+            local_vars: HashMap::new(),
+            num_vars: 0,
+            method_def,
+        };
+
+        let args = graph.args_node();
+
+        if !method_def.is_static {
+            // TODO `this`-ptr graph.set_value(0, &args.project(unsafe { mode::P }, 0));
+            se1f.num_vars += 1;
+
+            for (i, p) in method_def.params.iter().enumerate() {
+                let mode = get_firm_mode(&p.ty).expect("args mustn't have void type");
+                graph.set_value(se1f.new_local_var(p.name, mode), &args.project(mode, i + 1));
+            }
+        }
+
+        // TODO remove
+        //unsafe { keep_alive(graph.start_block().into()) };
+        //unsafe { keep_alive(graph.end_block().into()) };
+
+        se1f
+    }
+
+    /// Generate IR for a method body
+    fn gen_method(&mut self, body: &Spanned<'src, ast::Block<'src>>) {
+        unsafe { self.graph.set_cur_block(self.graph.start_block()) };
+        self.gen_block(body);
+        self.graph.start_block().mature();
+
+        // Void functions have an implicit return in the end
+        if self.method_def.return_ty == CheckedType::Void {
+            let mem = self.graph.cur_store();
+            let ret = self.graph.cur_block().new_return(mem, None);
+            self.graph.end_block().add_pred(&ret);
+        }
+
+        self.graph.end_block().mature();
+    }
+
+    /// Generate IR for a whole block
+    fn gen_block(&mut self, block: &Spanned<'src, ast::Block<'src>>) {
+        for stmt in &block.statements {
+            self.gen_stmt(&stmt);
+        }
+    }
+
+    /// Generate IR for a single statement
+    fn gen_stmt(&mut self, stmt: &Spanned<'src, ast::Stmt<'src>>) {
+        use self::ast::Stmt::*;
+        match &**stmt {
+            Block(block) => self.gen_block(block),
+            If(cond, then_arm, else_arm) => {
+                let prev_block = self.graph.cur_block();
+
+                // TODO Do we really need an additional header_block? Can't we just put the
+                // new_cond in the prev_block?
+                let incoming_jmp = prev_block.new_jmp();
+                let header_block = self.graph.new_imm_block(&incoming_jmp);
+
+                prev_block.mature(); // This block is done now
+                unsafe { self.graph.set_cur_block(header_block) };
+
+                // We evaluate the condition
+                let cond = header_block.new_cond(&self.gen_cond_expr(cond));
+
+                // If its true, we take the then_arm
+                let then_block = self.graph.new_imm_block(&cond.project_true());
+                {
+                    unsafe { self.graph.set_cur_block(then_block) };
+                    self.gen_stmt(&**then_arm);
+                }
+
+                // If its false, we take the else_arm
+                // We generate an else_block in any case, when there is no `else`, it's empty
+                let else_block = self.graph.new_imm_block(&cond.project_false());
+                if let Some(else_arm) = else_arm {
+                    unsafe { self.graph.set_cur_block(else_block) };
+                    self.gen_stmt(&**else_arm);
+                }
+
+                header_block.mature();
+
+                let from_then_jmp = then_block.new_jmp();
+                let from_else_jmp = else_block.new_jmp();
+
+                // Now we close the if-diamond
+                let next_block = self.graph.new_imm_block(&from_then_jmp);
+                next_block.add_pred(&from_else_jmp);
+                unsafe { self.graph.set_cur_block(next_block) };
+
+                // Those blocks are finished now
+                then_block.mature();
+                else_block.mature();
+            }
+
+            While(cond, body) => unimplemented!(),
+
+            Expression(expr) => {
+                self.gen_expr(expr);
+            }
+
+            Return(res_expr) => {
+                let mem = self.graph.cur_store();
+                let res = res_expr.as_ref().map(|res_expr| self.gen_expr(&*res_expr));
+
+                let ret = self.graph.cur_block().new_return(mem, res);
+
+                self.graph.end_block().add_pred(&ret);
+            }
+
+            LocalVariableDeclaration(_ty, name, init_expr) => {
+                // TODO here we need hennings type_analysis, because _ty is not a CheckedType.
+                // For now, just assume i32
+                //let mode = get_firm_mode(_ty).expect(&format!("var '{}' is void", name));
+                let mode = unsafe { mode::Is };
+                let var_slot = self.new_local_var(**name, mode);
+                if let Some(init_expr) = init_expr {
+                    self.graph.set_value(var_slot, &self.gen_expr(init_expr));
+                }
+            }
+
+            Empty => (),
+        }
+    }
+
+    /// Return a node that evaluates the given expression
+    ///
+    /// TODO non-raw-ptr abstraction for ret type; Box<dyn ValueNode> might
+    /// work, but unnecessary box
+    fn gen_expr(&mut self, expr: &Spanned<'src, ast::Expr<'src>>) -> *mut ir_node {
+        use self::ast::Expr::*;
+        match &**expr {
+            Int(lit) => {
+                let val = unsafe { new_tarval_from_long(lit.parse().unwrap(), mode::Is) };
+                self.graph.new_const(val).as_value_node()
+            }
+            Var(name) => {
+                let (slot, mode) = self.local_var(**name);
+                self.graph.value(slot, mode).as_value_node()
+            }
+
+            _ => unimplemented!(),
+        }
+    }
+
+    /// Assume an expression can be evaluated as a boolean and generate a
+    /// `Selector` for it
+    fn gen_cond_expr(&mut self, expr: &Spanned<'src, ast::Expr<'src>>) -> Selector {
+        use self::ast::Expr::*;
+        match &**expr {
+            Var(name) => {
+                let (slot, mode) = self.local_var(**name);
+                let val = self.graph.value(slot, mode);
+
+                let zero = self
+                    .graph
+                    .new_const(unsafe { new_tarval_from_long(0, mode) });
+
+                // cond is true iff. val != 0
+                self.graph
+                    .cur_block()
+                    // TODO ir_relation::NotEqual does not exist. Need ALU-neg?
+                    .new_cmp(&val, &zero, ir_relation::Greater)
+                    .as_selector()
+            }
+
+            _ => unimplemented!(),
+        }
+    }
+
+    /// Allocate a new local variable in the next free slot
+    fn new_local_var(&mut self, name: Symbol<'src>, mode: mode::Type) -> usize {
+        let slot = self.num_vars;
+        self.num_vars += 1;
+        self.local_vars.insert(name, (slot, mode));
+        slot
+    }
+
+    /// Get name and mode of previously allocated local var
+    fn local_var(&mut self, name: Symbol<'src>) -> (usize, mode::Type) {
+        self.local_vars
+            .get(&name)
+            .expect(&format!("undefined variable '{}'", name))
+            .clone()
     }
 }
 
@@ -129,6 +326,18 @@ fn get_firm_type(ty: &CheckedType<'_>) -> Option<Ty> {
         ),
         // Not possible
         CheckedType::Void | CheckedType::Null => None,
+    }
+}
+
+fn get_firm_mode(ty: &CheckedType<'_>) -> Option<mode::Type> {
+    match ty {
+        CheckedType::Int => Some(unsafe { mode::Is }),
+        CheckedType::Boolean => Some(unsafe { mode::Bu }),
+        CheckedType::TypeRef(_) | CheckedType::Array(_) | CheckedType::Null => {
+            Some(unsafe { mode::P })
+        }
+        // Not possible
+        CheckedType::Void => None,
     }
 }
 
