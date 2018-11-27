b4503b2153672b3a3d64eeca044da05af6dd4fb4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-27 22:29:55 +0000

    Deploy comprakt/comprakt to github.com/comprakt/comprakt.git:gh-pages

--- compiler_lib/type_checking/method_body_typechecker/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"enum":[["RefInfo",""],["VarDef",""]],"struct":[["CouldNotDetermineType",""],["ExprInfo",""],["MethodBodyTypeChecker",""]]});
--- runtime_rs/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"constant":[["SOURCE_CODE","rust source code of the MiniJava runtime"]]});
