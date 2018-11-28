adbc29baf3808287e13add996140cadf308bbda0
flip1995 <hello@philkrones.com>
2018-11-28 17:37:36 +0100

    Implement more arithmetic operations

--- compiler-lib/src/firm/method_body_generator.rs
+++ compiler-lib/src/firm/method_body_generator.rs
@@ -1,7 +1,7 @@
 use super::{Class, Runtime};
 use crate::{
     asciifile::Spanned,
-    ast,
+    ast::{self, BinaryOp},
     strtab::Symbol,
     type_checking::{
         type_analysis::TypeAnalysis,
@@ -218,31 +218,54 @@ impl<'a, 'ir, 'src, 'ast> MethodBodyGenerator<'ir, 'src, 'ast> {
                 let (slot, mode) = self.local_var(**name);
                 self.graph.value(slot, mode).as_value_node()
             }
-            Binary(ast::BinaryOp::Div, lhs, rhs) => {
+            Binary(op, lhs, rhs) => {
                 let lhs = self.gen_expr(lhs);
                 let rhs = self.gen_expr(rhs);
-                let mem = self.graph.cur_store();
-                log::debug!("pre new_div");
-                let div = self.graph.cur_block().new_div(mem, &lhs, &rhs, 0);
-                self.graph.set_store(div.project_mem());
-                log::debug!("pre project_res");
-                let res = div.project_res();
-                log::debug!("pre as_value_node");
-                res.as_value_node()
+                match op {
+                    BinaryOp::Add => {
+                        let add = self.graph.cur_block().new_add(&lhs, &rhs);
+                        add.as_value_node()
+                    }
+                    BinaryOp::Sub => {
+                        let sub = self.graph.cur_block().new_sub(&lhs, &rhs);
+                        sub.as_value_node()
+                    }
+                    BinaryOp::Mul => {
+                        let mul = self.graph.cur_block().new_mul(&lhs, &rhs);
+                        mul.as_value_node()
+
+                    }
+                    BinaryOp::Div => {
+                        let mem = self.graph.cur_store();
+                        log::debug!("pre new_div");
+                        let div = self.graph.cur_block().new_div(mem, &lhs, &rhs, 0);
+                        self.graph.set_store(div.project_mem());
+                        log::debug!("pre project_res");
+                        let res = div.project_res();
+                        log::debug!("pre as_value_node");
+                        res.as_value_node()
+                    }
+                    BinaryOp::Mod => {
+                        let mem = self.graph.cur_store();
+                        log::debug!("pre new_mod");
+                        let mod_node = self.graph.cur_block().new_mod(mem, &lhs, &rhs, 0);
+                        self.graph.set_store(mod_node.project_mem());
+                        log::debug!("pre project_res");
+                        let res = mod_node.project_res();
+                        log::debug!("pre as_value_node");
+                        res.as_value_node()
+                    }
+                    BinaryOp::LogicalOr => unimplemented!(),
+                    BinaryOp::LogicalAnd => unimplemented!(),
+                    BinaryOp::Assign => unimplemented!(),
+                    BinaryOp::Equals => unimplemented!(),
+                    BinaryOp::NotEquals => unimplemented!(),
+                    BinaryOp::LessThan => unimplemented!(),
+                    BinaryOp::GreaterThan => unimplemented!(),
+                    BinaryOp::LessEquals => unimplemented!(),
+                    BinaryOp::GreaterEquals => unimplemented!(),
+                }
             }
-            Binary(ast::BinaryOp::Mod, lhs, rhs) => {
-                let lhs = self.gen_expr(lhs);
-                let rhs = self.gen_expr(rhs);
-                let mem = self.graph.cur_store();
-                log::debug!("pre new_mod");
-                let mod_node = self.graph.cur_block().new_mod(mem, &lhs, &rhs, 0);
-                self.graph.set_store(mod_node.project_mem());
-                log::debug!("pre project_res");
-                let res = mod_node.project_res();
-                log::debug!("pre as_value_node");
-                res.as_value_node()
-            }
-            Binary(_op, _expr_left, _expr_right) => unimplemented!(),
             Unary(ast::UnaryOp::Neg, expr) => {
                 let expr = self.gen_expr(expr);
                 log::debug!("pre new_neg");
