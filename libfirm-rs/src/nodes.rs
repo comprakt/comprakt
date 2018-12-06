b574e91a7055891422f8d5fc27ba3a4245039657
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2018-12-06 16:50:49 +0100

    add methods to get reverse edges to NodeTrait

--- libfirm-rs/src/nodes.rs
+++ libfirm-rs/src/nodes.rs
@@ -17,8 +17,57 @@ pub trait NodeTrait {
         }
     }
 
+    // TODO: should we use dynamic reverse edges instead of reverse
+    fn reverse_edges(&self) -> Vec<Node> {
+        let id = self.internal_ir_node();
+        let num_outs = unsafe { bindings::get_irn_n_outs(id) };
+
+        let mut outs = Vec::new();
+
+        for out_idx in 0..num_outs {
+            let out = unsafe { bindings::get_irn_out(id, out_idx) };
+            outs.push(NodeFactory::node(out));
+        }
+
+        outs
+    }
+
+    fn reverse_edge_iterator(&self) -> ReverseEdgeIterator {
+        ReverseEdgeIterator::new(self.internal_ir_node())
+    }
+
     // TODO implement methods from
     // https://github.com/libfirm/jFirm/blob/master/src/firm/nodes/Node.java
+}
+
+pub struct ReverseEdgeIterator {
+    node: *mut bindings::ir_node,
+    cur: u32,
+    len: u32,
+}
+
+impl ReverseEdgeIterator {
+    fn new(node: *mut bindings::ir_node) -> Self {
+        Self {
+            node,
+            len: unsafe { bindings::get_irn_n_outs(node) },
+            cur: 0,
+        }
+    }
+}
+
+impl Iterator for ReverseEdgeIterator {
+    type Item = Node;
+
+    fn next(&mut self) -> Option<Node> {
+        if self.cur == self.len {
+            None
+        } else {
+            let out = unsafe { bindings::get_irn_out(self.node, self.cur) };
+            self.cur += 1;
+            Some(NodeFactory::node(out))
+        }
+    }
 }
 
 // TODO maybe Into<*const ir_node> for NodeTrait?
