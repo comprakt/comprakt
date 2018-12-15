3f70caaca62f83c0200dfe1fbb2f1aec1690247d
Joshua Bachmeier <joshua@bachmeier.cc>
2018-12-15 17:52:18 +0100

    lir: Implement value transfer construction
    Does not seem to work for loops

--- compiler-lib/src/lowering/lir.rs
+++ compiler-lib/src/lowering/lir.rs
@@ -81,7 +81,7 @@ pub struct BlockGraph {
 #[derive(Debug)]
 pub struct BasicBlock {
     /// The Pseudo-registers used by the Block
-    pub regs: Vec<ValueSlot>,
+    pub regs: Vec<Vec<MutRc<ValueSlot>>>,
     /// The instructions (using arbitrarily many registers) of the block
     pub code: Vec<Instruction>,
     /// Control flow-transfers *to* this block.
@@ -101,24 +101,49 @@ pub type Instruction = !;
 /// An abstract pseudo-register
 #[derive(Debug)]
 pub struct ValueSlot {
-    /// The block in which this slot is allocated
-    //block: MutWeak<BasicBlock>,
     /// The slot number. Uniqe only per Block, not globally
     num: usize,
-    kind: ValueSlotKind,
     /// The firm node that corresponds to this value
     firm: Node,
+
+    /// The block in which this slot is allocated
+    allocated_in: MutWeak<BasicBlock>,
+    /// The block in which the value of this slot originates
+    originates_in: MutWeak<BasicBlock>,
+    /// The block in which this value is used
+    terminates_in: MutWeak<BasicBlock>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Display)]
 pub enum ValueSlotKind {
-    /// The value in this slot is used in the instructions of the block
-    Used,
-    /// Ghost slots are unused in this block, but contain values that must be
+    /// The value in this slot originates in the local block, but is used in a
+    /// later block
+    ///
+    /// Original values must appear in the transitions of all `succs`.
+    #[display(fmt = "original")]
+    Original,
+    /// The value in this slot is used in the instructions of the local block,
+    /// but originiates in a previous block
+    ///
+    /// Terminal values must appear in the transitions of all `preds`.
+    #[display(fmt = "terminal")]
+    Terminal,
+    /// The slot is unused in the local block, but contains a value that must be
     /// kept alive for the duration of this block. This is created for values
     /// that are calculated in an earlier block, and used in a later block (but
     /// not in this block).
-    Ghost,
+    ///
+    /// Pass-through values must appear in the transitions both all `preds` and
+    /// `succs`.
+    #[display(fmt = "pass-through")]
+    PassThrough,
+    /// The value of the slot both originates and is used (terminates) in the
+    /// local block.
+    ///
+    /// Private values must appear neither in the transitions of any `preds`,
+    /// nor `succs`.
+    #[display(fmt = "private")]
+    Private,
 }
 
 /// Transfer control-flow from one block to another. This is an edge in the
@@ -147,7 +172,7 @@ pub struct ControlFlowTransfer {
     ///
     /// The *swap problem* is handled by there being no semantic order between
     /// the register transitions.
-    register_transitions: Vec<(ValueSlot, ValueSlot)>,
+    register_transitions: Vec<(MutRc<ValueSlot>, MutRc<ValueSlot>)>,
 
     source: MutWeak<BasicBlock>,
     pub target: MutRc<BasicBlock>,
@@ -156,7 +181,6 @@ pub struct ControlFlowTransfer {
 impl From<libfirm_rs::graph::Graph> for BlockGraph {
     fn from(firm_graph: libfirm_rs::graph::Graph) -> Self {
         let mut graph = Self::build_skeleton(firm_graph);
-        log::debug!("Head: {:?}", graph.head.borrow().firm);
         graph.fill_blocks();
         graph
     }
@@ -180,8 +204,6 @@ impl BlockGraph {
                         target: MutRc::clone(&target),
                     });
 
-                    log::debug!("Visiting edge: {:?}->{:?}", firm_source, firm_target);
-
                     source.borrow_mut().succs.push(MutRc::clone(&edge));
                     target.borrow_mut().preds.push(MutRc::downgrade(&edge));
                 }
@@ -204,24 +226,74 @@ impl BlockGraph {
     }
 
     fn fill_blocks(&mut self) {
-        self.firm.walk_blocks(|visit, firm_target| match visit {
+        self.firm.walk_blocks(|visit, firm_block| match visit {
             VisitTime::BeforePredecessors => {
-                log::debug!("Nodes in: {:?}", firm_target);
-                for node_in_block in firm_target.out_nodes() {
-                    log::debug!("\tGreen points of {:?}", node_in_block);
+                log::debug!("VISIT {:?}", firm_block);
+                let local_block = self.get_block(*firm_block);
+
+                // Foreign values are the green points in yComp (inter-block edges)
+                for node_in_block in firm_block.out_nodes() {
+                    log::debug!("---");
                     match node_in_block {
                         Node::Phi(phi) => {
-                            for (cfg_pred, value) in phi.preds() {
-                                log::debug!("\t\t {:?} via {:?}", value, cfg_pred);
-                            }
+                            let multislot = local_block.new_terminating_multislot();
+                            phi.preds()
+                                // What TODO with mem edges?
+                                .filter(|(_, value)| {
+                                    value.mode() != unsafe { libfirm_rs::bindings::mode::M }
+                                })
+                                // The next two 'maps' need to be seperated in two closures
+                                // because we wan't to selectively `move` `value` and
+                                // `multislot` into the closure, but take `self` by reference
+                                .map(|(cfg_pred, value)| {
+                                    (
+                                        cfg_pred,
+                                        value,
+                                        MutRc::downgrade(&self.get_block(value.block())),
+                                    )
+                                })
+                                .map(move |(cfg_pred, value, original_block)| {
+                                    (
+                                        cfg_pred,
+                                        multislot.add_possible_value(value, original_block),
+                                    )
+                                })
+                                .for_each(|(cfg_pred, value_slot)| {
+                                    log::debug!("");
+                                    local_block
+                                        .borrow()
+                                        .find_incoming_edge_from(cfg_pred)
+                                        .unwrap()
+                                        .borrow_mut()
+                                        .add_incoming_value_flow(value_slot);
+                                });
                         }
-                        _ => {
-                            for child in node_in_block.in_nodes() {
-                                if child.block() != *firm_target {
-                                    log::debug!("\t\t{:?}", child)
+
+                        _ => node_in_block
+                            .in_nodes()
+                            // What TODO with mem edges?
+                            .filter(|value| {
+                                value.mode() != unsafe { libfirm_rs::bindings::mode::M }
+                            })
+                            .filter_map(|value| {
+                                if value.block() != *firm_block {
+                                    Some(value)
+                                } else {
+                                    // This is a value produced by our block, so no need to get
+                                    // it from somewhere
+                                    None
                                 }
-                            }
-                        }
+                            })
+                            // Foreign values that are not phi, flow in from each cfg pred
+                            // => values x cfg_preds
+                            .for_each(|value| {
+                                log::debug!("");
+                                // Do this here, because we don't want to move `self` or
+                                // `local_block` into closure
+                                let original_block = self.get_block(value.block());
+                                let local_block = MutRc::clone(&local_block);
+                                local_block.new_terminating_slot(value, original_block.downgrade());
+                            }),
                     }
                 }
             }
@@ -229,9 +301,130 @@ impl BlockGraph {
             VisitTime::AfterPredecessors => (),
         });
     }
+
+    fn get_block(&self, firm_block: libfirm_rs::nodes::Block) -> MutRc<BasicBlock> {
+        self.blocks
+            .get(&firm_block)
+            .expect("BlockGraph is incomplete")
+            .clone()
+    }
+}
+
+impl ControlFlowTransfer {
+    /// Only call this from target
+    fn add_incoming_value_flow(&mut self, target_slot: MutRc<ValueSlot>) {
+        let source_slot = self
+            .source
+            .upgrade()
+            .unwrap()
+            .new_forwarding_slot(&target_slot.borrow());
+
+        assert_eq!(target_slot.borrow().firm, source_slot.borrow().firm);
+        log::debug!(
+            "\tTRANSFER: from='{:?}' to='{:?}' value='{:?}'",
+            upborrow!(self.source).firm,
+            self.target.borrow().firm,
+            target_slot.borrow().firm,
+        );
+
+        self.register_transitions.push((source_slot, target_slot));
+    }
+}
+
+impl MutRc<BasicBlock> {
+    fn new_multislot(&self, terminates_in: MutWeak<BasicBlock>) -> MultiSlotBuilder {
+        MultiSlotBuilder::new(MutRc::clone(self), terminates_in)
+    }
+
+    fn new_terminating_multislot(&self) -> MultiSlotBuilder {
+        self.new_multislot(MutRc::downgrade(self))
+    }
+
+    fn new_slot(
+        &self,
+        value: libfirm_rs::nodes::Node,
+        originates_in: MutWeak<BasicBlock>,
+        terminates_in: MutWeak<BasicBlock>,
+    ) -> MutRc<ValueSlot> {
+        let this = self.borrow();
+        let possibly_existing_slot = this
+            .regs
+            .iter()
+            .filter_map(|multislot| multislot.iter().find(|slot| slot.borrow().firm == value))
+            .next();
+
+        let slot = if let Some(slot) = possibly_existing_slot {
+            log::debug!(
+                "\tREUSE: slot={} in='{:?}' kind={} value='{:?}'",
+                slot.borrow().num,
+                this.firm,
+                slot.borrow().value_kind(),
+                slot.borrow().firm
+            );
+
+            MutRc::clone(slot)
+        } else {
+            drop(possibly_existing_slot);
+            drop(this);
+            self.new_multislot(terminates_in)
+                .add_possible_value(value, originates_in)
+        };
+
+        // If the value is foreign, we need to "get it" from each blocks above us,
+        // to pass it through to the block below us. This only happens if this function
+        // is called from `ControlFlowtransfer::add_incoming_value_flow`.
+        if !slot.borrow().value_kind().originates_here() {
+            for pred in &self.borrow().preds {
+                upborrow!(mut pred).add_incoming_value_flow(MutRc::clone(&slot));
+            }
+        }
+
+        slot
+    }
+
+    fn new_forwarding_slot(&self, target_slot: &ValueSlot) -> MutRc<ValueSlot> {
+        let local_slot = self.new_slot(
+            target_slot.firm,
+            MutWeak::clone(&target_slot.originates_in),
+            MutWeak::clone(&target_slot.terminates_in),
+        );
+
+        assert!(!local_slot.borrow().value_kind().terminates_here());
+        local_slot
+    }
+
+    fn new_terminating_slot(
+        &self,
+        value: libfirm_rs::nodes::Node,
+        origin: MutWeak<BasicBlock>,
+    ) -> MutRc<ValueSlot> {
+        let local_slot = self.new_slot(value, origin, MutRc::downgrade(self));
+
+        assert!(local_slot.borrow().value_kind().terminates_here());
+        local_slot
+    }
+
+    #[allow(dead_code)]
+    fn new_private_slot(&self, value: libfirm_rs::nodes::Node) -> MutRc<ValueSlot> {
+        assert_eq!(value.block(), self.borrow().firm);
+        let slot = self.new_slot(value, MutRc::downgrade(self), MutRc::downgrade(self));
+
+        assert!(slot.borrow().value_kind().is_private());
+        slot
+    }
 }
 
 impl BasicBlock {
+    fn find_incoming_edge_from(
+        &self,
+        cfg_pred: libfirm_rs::nodes::Block,
+    ) -> Option<MutRc<ControlFlowTransfer>> {
+        self.preds
+            .iter()
+            .map(|edge| MutWeak::upgrade(edge).unwrap())
+            .find(|edge| upborrow!(edge.borrow().source).firm == cfg_pred)
+    }
+
     fn skeleton_block(
         known_blocks: &mut HashMap<libfirm_rs::nodes::Block, MutRc<BasicBlock>>,
         firm: libfirm_rs::nodes::Block,
@@ -248,5 +441,112 @@ impl BasicBlock {
                 })
             })
             .clone()
+    }
+}
+
+#[derive(Debug)]
+pub struct MultiSlotBuilder {
+    num: usize,
+    allocated_in: MutWeak<BasicBlock>,
+    terminates_in: MutWeak<BasicBlock>,
+}
+
+impl MultiSlotBuilder {
+    fn new(allocated_in: MutRc<BasicBlock>, terminates_in: MutWeak<BasicBlock>) -> Self {
+        let num = allocated_in.borrow().regs.len();
+        allocated_in.borrow_mut().regs.push(Vec::new());
+        MultiSlotBuilder {
+            num,
+            allocated_in: MutRc::downgrade(&allocated_in),
+            terminates_in,
+        }
+    }
+
+    fn add_possible_value(
+        &self,
+        value: libfirm_rs::nodes::Node,
+        originates_in: MutWeak<BasicBlock>,
+    ) -> MutRc<ValueSlot> {
+        assert!(upborrow!(self.allocated_in).regs.get(self.num).is_some());
+
+        {
+            let allocated_in = MutWeak::upgrade(&self.allocated_in).unwrap();
+            let allocated_in = allocated_in.borrow();
+            let is_duplicate = allocated_in.regs[self.num]
+                .iter()
+                .any(|slot| slot.borrow().firm == value);
+
+            assert!(!is_duplicate);
+        }
+
+        let slot = ValueSlot {
+            num: self.num,
+            firm: value,
+            allocated_in: MutWeak::clone(&self.allocated_in),
+            originates_in,
+            terminates_in: MutWeak::clone(&self.terminates_in),
+        };
+
+        log::debug!(
+            "\tALLOC: slot={} in='{:?}' kind={} value='{:?}'",
+            slot.num,
+            upborrow!(self.allocated_in).firm,
+            slot.value_kind(),
+            slot.firm
+        );
+
+        let slot = MutRc::new(slot);
+        upborrow!(mut self.allocated_in).regs[self.num].push(MutRc::clone(&slot));
+
+        slot
+    }
+}
+
+impl ValueSlot {
+    pub fn value_kind(&self) -> ValueSlotKind {
+        use self::ValueSlotKind::*;
+        if upborrow!(self.originates_in).firm == upborrow!(self.terminates_in).firm {
+            assert_eq!(
+                upborrow!(self.allocated_in).firm,
+                upborrow!(self.originates_in).firm
+            );
+            Private
+        } else if upborrow!(self.allocated_in).firm == upborrow!(self.originates_in).firm {
+            Original
+        } else if upborrow!(self.allocated_in).firm == upborrow!(self.terminates_in).firm {
+            Terminal
+        } else {
+            PassThrough
+        }
+    }
+}
+
+impl ValueSlotKind {
+    pub fn is_private(self) -> bool {
+        use self::ValueSlotKind::*;
+        self == Private
+    }
+
+    pub fn is_original(self) -> bool {
+        use self::ValueSlotKind::*;
+        self == Original
+    }
+
+    pub fn is_terminal(self) -> bool {
+        use self::ValueSlotKind::*;
+        self == Terminal
+    }
+
+    pub fn is_pass_through(self) -> bool {
+        use self::ValueSlotKind::*;
+        self == PassThrough
+    }
+
+    pub fn terminates_here(self) -> bool {
+        self.is_private() || self.is_terminal()
+    }
+
+    pub fn originates_here(self) -> bool {
+        self.is_private() || self.is_original()
     }
 }
