5610ec052aaa15b91c5c1523350ef036d29022f7
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2019-01-03 22:08:56 +0100

    actually remove detected unnecessary jmp chains

--- compiler-lib/src/optimization/jump_threading.rs
+++ compiler-lib/src/optimization/jump_threading.rs
@@ -102,6 +102,15 @@ impl JumpThreading {
 
             match current {
                 Node::Jmp(jmp) => {
+                    // In theory, we have to recompute the outs after each
+                    // rewiring. But given the access pattern in the code
+                    // below, recomputing once per iteration is sufficient.
+                    //
+                    // Note that this is necessary for the correctness of
+                    // - block.out_nodes()
+                    // - jmp.out_target_block()
+                    self.graph.assure_outs();
+
                     breakpoint!("Jump Threading: is jump!", self.graph, &|node: &Node| {
                         let mut label = default_label(node);
 
@@ -116,15 +125,16 @@ impl JumpThreading {
                     });
 
                     let block = current.block();
+                    let block_node = Node::Block(block);
 
                     if block.out_nodes().len() == 1 {
-                        // the block contains only a single out node,
+                        // the block contains only a single node,
                         // which we know is the Jmp Node, so the block
                         // can be removed
                         breakpoint!("Block can be removed!", self.graph, &|node: &Node| {
                             let mut label = default_label(node);
 
-                            if node == &current || node == &Node::Block(block) {
+                            if node == &current || node == &block_node {
                                 label = label
                                     .style(Style::Filled)
                                     .fillcolor(X11Color::Orange)
@@ -135,10 +145,101 @@ impl JumpThreading {
                         });
                     }
 
-                    worklist.push(Node::Block(block));
+                    if let Some(target_block) = jmp.out_target_block() {
+                        //let target_pred_idx = target_block.in_nodes().position(|pred| pred == Node::Jmp(jmp)).unwrap();
+                        //log::debug!("at idx: {}", target_pred_idx);
+                        
+                        // remove the unecessary jump from the input edges of the block it
+                        // points to.
+                        let mut new_inputs = target_block
+                            .in_nodes()
+                            .filter(|pred| pred != &Node::Jmp(jmp))
+                            .collect::<Vec<_>>();
+
+                        // we can only deal with some predecessors, only change
+                        // the graph when we know how to deal with every predecessor
+                        if block.cfg_preds().all(|node| {
+                            match node {
+                                Node::Jmp(_) | Node::Proj(_, _) => true,
+                                unsupported => {
+                                    log::debug!("skipping jump threading opportunity because of unsupported predecessor variant {:?}", unsupported);
+                                    false
+                                }
+                            }
+                        }) {
+
+                            // rewire the block and add its children to
+                            // the list of nodes that should be visited
+                            for node in block.cfg_preds() {
+                                match node {
+                                    Node::Jmp(pred_jmp) => {
+                                        log::debug!("Rewiring {:?} to directly point to {:?}", pred_jmp, target_block);
+                                        // the block contains only a single node,
+                                        // which we know is the Jmp Node, so the block
+                                        // can be removed
+                                        //let new_jmp = node.block().new_jmp();
+                                        //unsafe { bindings::set_irn_n(target_block.internal_ir_node(), target_pred_idx as i32, pred_jmp.internal_ir_node()); }
+                                        new_inputs.push(node);
+                                        target_block.set_in_nodes(&new_inputs);
+
+                                        breakpoint!(
+                                            &format!("Rewiring {:?} to directly point to {:?}", pred_jmp, target_block),
+                                            self.graph, &|rendered: &Node| {
+                                            let mut label = default_label(rendered);
+
+                                            if rendered == &node || rendered == &Node::Block(target_block) {
+                                                label = label
+                                                    .style(Style::Filled)
+                                                    .fillcolor(X11Color::Brown)
+                                                    .fontcolor(X11Color::White);
+                                            }
+
+                                            label
+                                        });
+                                    }
+                                    Node::Proj(proj, _kind) => {
+                                        log::debug!("Rewiring {:?} to directly point to {:?}", proj, target_block);
+
+                                        new_inputs.push(node);
+                                        target_block.set_in_nodes(&new_inputs);
+
+                                        breakpoint!(
+                                            &format!("Rewiring {:?} to directly point to {:?}", proj, target_block),
+                                            self.graph, &|rendered: &Node| {
+                                            let mut label = default_label(rendered);
+
+                                            if rendered == &node || rendered == &Node::Block(target_block) {
+                                                label = label
+                                                    .style(Style::Filled)
+                                                    .fillcolor(X11Color::Brown)
+                                                    .fontcolor(X11Color::White);
+                                            }
+
+                                            label
+                                        });
+                                    }
+                                    _ => unreachable!()
+                                }
+
+                                worklist.push(node);
+                            }
+
+                            // TODO: Is this necessary?
+                            // mark the block as bad, it will be removed
+                            // with all nodes it dominates (which is only the
+                            // currently observed, unnecessary jmp)
+                            self.graph.mark_as_bad(block_node);
+                            self.graph.remove_bads();
+                        } else {
+                            // queueing the Jmp Node failed, treat it as all
+                            // other nodes
+                            worklist.push(Node::Block(current.block()));
+                        }
+                    } else {
+                        log::debug!("ignoring reducable jump without a target block");
+                    }
                 }
                 Node::Block(block) => {
-                    log::debug!("cfg preds: {:?}", block.cfg_preds().collect::<Vec<_>>());
                     // add all nodes that jump to the current block
                     // to the work list
                     worklist.extend(block.cfg_preds());
@@ -163,4 +264,5 @@ impl JumpThreading {
 
         Outcome::Unchanged
     }
+
 }
