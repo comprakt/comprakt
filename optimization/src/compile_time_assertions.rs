8e6cf4d822e9ca30e631de22dc3910e25d694aaa
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2019-02-05 15:03:53 +0100

    add compile time assertion to integration tests

---
+++ optimization/src/compile_time_assertions.rs
@@ -0,0 +1,126 @@
+//! Injects the class 'Assert' into the mini java runtime, which
+//! contains functions that are evaluated at compile-time.
+use crate::{firm::FirmProgram, optimization};
+use libfirm_rs::{
+    nodes::{self, Node},
+    Graph,
+};
+use std::fmt;
+
+#[derive(Default, Clone, Debug)]
+pub struct CompileTimeAssertions {}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Phase {
+    pub opt_idx: usize,
+    pub opt_kind: optimization::Kind,
+    pub is_already_applied: bool,
+}
+
+impl fmt::Display for Phase {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{} optimization number {}. {}",
+            if self.is_already_applied {
+                "after"
+            } else {
+                "before"
+            },
+            self.opt_idx + 1,
+            self.opt_kind
+        )
+    }
+}
+
+impl Phase {
+    fn is_match(&self, filter: &str) -> bool {
+        if self.opt_kind.to_string() == filter {
+            return true;
+        }
+
+        if let Ok(num) = filter.parse::<usize>() {
+            return self.opt_idx == num;
+        }
+
+        return false;
+    }
+}
+
+impl CompileTimeAssertions {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn check_program(&self, program: &FirmProgram<'_, '_>, phase: Phase) {
+        log::debug!("checking assertions for phase {:?}", phase);
+
+        for method in program.methods.values() {
+            if let Some(graph) = method.borrow().graph {
+                self.check_graph(graph, phase);
+            }
+        }
+    }
+
+    // check all assertions, panic if the an assertion is
+    // wrong.
+    pub fn check_graph(&self, graph: Graph, phase: Phase) {
+        graph.walk(|node| {
+            if let Some(call) = Node::as_call(*node) {
+                if let Some(method_name) = call.method_name() {
+                    match method_name.as_str() {
+                        "Assert$M$is_const_after" if phase.is_already_applied => {
+                            assert_const(call, &phase)
+                        }
+                        "Assert$M$is_const_before" if !phase.is_already_applied => {
+                            assert_const(call, &phase)
+                        }
+                        "Assert$M$is_not_const_after" if phase.is_already_applied => {
+                            assert_not_const(call, &phase)
+                        }
+                        "Assert$M$is_not_const_before" if !phase.is_already_applied => {
+                            assert_not_const(call, &phase)
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        });
+    }
+}
+
+fn assert_const(call: nodes::Call, phase: &Phase) {
+    let phase_filter_node = call.args().nth(1).unwrap();
+    // TODO: somehow suppport specifying optimizations by name instead of indices
+    let phase_filter = Node::as_const(phase_filter_node)
+        .unwrap()
+        .tarval()
+        .get_long()
+        .to_string();
+    let node = call.args().nth(2).unwrap();
+
+    if phase.is_match(&phase_filter) {
+        assert!(
+            Node::is_const(node),
+            format!("expected {:?} to be a constant {}", node, phase)
+        );
+    }
+}
+
+fn assert_not_const(call: nodes::Call, phase: &Phase) {
+    let phase_filter_node = call.args().nth(1).unwrap();
+    // TODO: somehow suppport specifying optimizations by name instead of indices
+    let phase_filter = Node::as_const(phase_filter_node)
+        .unwrap()
+        .tarval()
+        .get_long()
+        .to_string();
+    let node = call.args().nth(2).unwrap();
+
+    if phase.is_match(&phase_filter) {
+        assert!(
+            !Node::is_const(node),
+            format!("expected {:?} to NOT be a constant {}", node, phase)
+        );
+    }
+}
