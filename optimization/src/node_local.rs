95f46b0c86af89069bb3be13b878ab0782e41f89
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2019-02-09 17:43:57 +0100

    compatibility with constant folding, make Conv in front of divisor optional

--- optimization/src/node_local.rs
+++ optimization/src/node_local.rs
@@ -30,7 +30,7 @@ impl NodeLocal {
     fn visit_node(&mut self, current_node: Node) {
         // do not put a breakpoint here, this will result in a walker in walker
         // which just does random shit in libfirm.
-        log::debug!("Div2Shift: visiting {:?}", current_node);
+        log::debug!("LO: visiting {:?}", current_node);
 
         #[allow(clippy::single_match)]
         match current_node {
@@ -43,7 +43,7 @@ impl NodeLocal {
                 if left == right {
                     // convert (a + a) to (a << 2)
                     log::debug!(
-                        "Div2Shift: self addition {:?} [left:{:?},right:{:?}] \
+                        "LO: self addition {:?} [left:{:?},right:{:?}] \
                          replaced by '{:?} << 2'",
                         add,
                         left,
@@ -100,7 +100,7 @@ impl NodeLocal {
                     };
 
                     log::debug!(
-                        "Div2Shift: {:?} [left:{:?},right:{:?}] replaced by '{:?} << {}'",
+                        "LO: Mul2Shift: {:?} [left:{:?},right:{:?}] replaced by '{:?} << {}'",
                         mul,
                         left,
                         right,
@@ -113,96 +113,119 @@ impl NodeLocal {
             }
             Node::Div(div) => {
                 log::debug!(
-                    "Div2Shift: {:?}[left:{:?},right:{:?}] ",
+                    "LO: Div2Shift: {:?}[left:{:?},right:{:?}] ",
                     div,
                     div.left(),
                     div.right(),
                 );
-                if let Node::Conv(conv) = div.right() {
-                    log::debug!("Div2Shift: conv {:?}!", conv);
-                    if let Node::Const(divisor) = conv.op() {
-                        log::debug!("Div2Shift: divisor {:?}!", divisor);
-                        if let TarvalKind::Long(divisor_value) = divisor.tarval().kind() {
-                            log::debug!("Div2Shift: divisor value {:?}!", divisor_value);
 
-                            // cannot crash since min_val is not a power of two
-                            let abs_divisor_value = divisor_value.abs() as u64;
-                            let has_minus = divisor_value < 0;
-
-                            if abs_divisor_value.is_power_of_two() {
-                                log::debug!("Div2Shift: is power of two!");
-                                let shift_amount = 64 - 1 - abs_divisor_value.leading_zeros();
-                                let shift_amount_node = self
-                                    .graph
-                                    .new_const(Tarval::val(i64::from(shift_amount), Mode::Iu()));
-
-                                let const_31 = self.graph.new_const(Tarval::val(31, Mode::Iu()));
-                                let const_1 = self.graph.new_const(Tarval::mj_int(1));
-
-                                let div_proj_res = if let Some(res) = div.out_proj_res() {
-                                    res
-                                } else {
-                                    return;
-                                };
-
-                                if div_proj_res.out_nodes().len() != 1 {
-                                    return;
-                                }
-
-                                // this is the conv hanging at the end of the div
-                                let div_end = div_proj_res.out_nodes().nth(0).unwrap();
-
-                                // reach through the conv node in front of the div
-                                if div.left().in_nodes().len() != 1 {
-                                    return;
-                                }
-
-                                let real_left = div.left().in_nodes().nth(0).unwrap();
-
-                                let block = div.block();
-                                let shr_by_31 = block.new_shrs(real_left, const_31);
-                                let shl_1_by_shift = block.new_shl(const_1, shift_amount_node);
-                                let shl_1_by_shift_minus_1 = block.new_sub(shl_1_by_shift, const_1);
-                                let binary_and = block.new_and(shr_by_31, shl_1_by_shift_minus_1);
-                                let add_binary_and = block.new_add(real_left, binary_and);
-                                let shift_to_result =
-                                    block.new_shrs(add_binary_and, shift_amount_node);
-
-                                let shr_end = if has_minus {
-                                    Node::Minus(div.block().new_minus(shift_to_result))
-                                } else {
-                                    Node::Shrs(shift_to_result)
-                                };
-
-                                log::debug!(
-                                    "Div2Shift: memory edge through div {:?} is {:?} -> {:?}",
-                                    div,
-                                    div.mem(),
-                                    div.out_proj_m(),
-                                );
-
-                                let div_proj_mem = if let Some(mem) = div.out_proj_m() {
-                                    mem
-                                } else {
-                                    return;
-                                };
-
-                                // drop the mem edge from the div
-                                Graph::exchange(div_proj_mem, div.mem());
-
-                                log::debug!(
-                                    "Div2Shift: {:?}[left:{:?},right:{:?}] replaced by '>> {}'",
-                                    div,
-                                    div.left(),
-                                    div.right(),
-                                    shift_amount
-                                );
-
-                                Graph::exchange(div_end, shr_end);
-
-                                self.changed = Outcome::Changed;
-                            }
+                // we expect either
+                // - a Conv with a Const Is as operand [which is the output of firm
+                //   construction]
+                // - or a constant with Const Ls [which is the output of constant folding]
+                let divisor = match div.right() {
+                    Node::Conv(conv) => {
+                        log::debug!("Div2Shift: with conv {:?}!", conv);
+                        if let Node::Const(divisor) = conv.op() {
+                            divisor
+                        } else {
+                            return;
                         }
+                    }
+                    Node::Const(divisor) => divisor,
+                    _ => {
+                        return;
+                    }
+                };
+
+                log::debug!("Div2Shift: divisor {:?}!", divisor);
+
+                if let TarvalKind::Long(divisor_value) = divisor.tarval().kind() {
+                    log::debug!("Div2Shift: divisor value {:?}!", divisor_value);
+
+                    if divisor_value > std::i32::MAX.into() || divisor_value < std::i32::MIN.into()
+                    {
+                        log::debug!("Div2Shift: aborting since value is not in i32 range!",);
+                        // this can only happen through constant folding, which might
+                        // generate a Const Ls out of the i32 range
+                        return;
+                    }
+
+                    // cannot crash since min_val is not a power of two
+                    let abs_divisor_value = divisor_value.abs() as u64;
+                    let has_minus = divisor_value < 0;
+
+                    if abs_divisor_value.is_power_of_two() {
+                        log::debug!("Div2Shift: is power of two!");
+                        let shift_amount = 64 - 1 - abs_divisor_value.leading_zeros();
+                        let shift_amount_node = self
+                            .graph
+                            .new_const(Tarval::val(i64::from(shift_amount), Mode::Iu()));
+
+                        let const_31 = self.graph.new_const(Tarval::val(31, Mode::Iu()));
+                        let const_1 = self.graph.new_const(Tarval::mj_int(1));
+
+                        let div_proj_res = if let Some(res) = div.out_proj_res() {
+                            res
+                        } else {
+                            return;
+                        };
+
+                        if div_proj_res.out_nodes().len() != 1 {
+                            return;
+                        }
+
+                        // this is the conv hanging at the end of the div
+                        let div_end = div_proj_res.out_nodes().nth(0).unwrap();
+
+                        // reach through the conv node in front of the div
+                        if div.left().in_nodes().len() != 1 {
+                            return;
+                        }
+
+                        let real_left = div.left().in_nodes().nth(0).unwrap();
+
+                        let block = div.block();
+                        let shr_by_31 = block.new_shrs(real_left, const_31);
+                        let shl_1_by_shift = block.new_shl(const_1, shift_amount_node);
+                        let shl_1_by_shift_minus_1 = block.new_sub(shl_1_by_shift, const_1);
+                        let binary_and = block.new_and(shr_by_31, shl_1_by_shift_minus_1);
+                        let add_binary_and = block.new_add(real_left, binary_and);
+                        let shift_to_result = block.new_shrs(add_binary_and, shift_amount_node);
+
+                        let shr_end = if has_minus {
+                            Node::Minus(div.block().new_minus(shift_to_result))
+                        } else {
+                            Node::Shrs(shift_to_result)
+                        };
+
+                        log::debug!(
+                            "LO: Div2Shift: memory edge through div {:?} is {:?} -> {:?}",
+                            div,
+                            div.mem(),
+                            div.out_proj_m(),
+                        );
+
+                        let div_proj_mem = if let Some(mem) = div.out_proj_m() {
+                            mem
+                        } else {
+                            return;
+                        };
+
+                        // drop the mem edge from the div
+                        Graph::exchange(div_proj_mem, div.mem());
+
+                        log::debug!(
+                            "LO: Div2Shift: {:?}[left:{:?},right:{:?}] replaced by '>> {}'",
+                            div,
+                            div.left(),
+                            div.right(),
+                            shift_amount
+                        );
+
+                        Graph::exchange(div_end, shr_end);
+
+                        self.changed = Outcome::Changed;
                     }
                 }
             }
