39f41306327e93f2b5ae27d97f7e46aaa24473a2
flip1995 <hello@philkrones.com>
2019-02-10 15:51:53 +0100

    Clippy compiler-shared

--- compiler-shared/src/lib.rs
+++ compiler-shared/src/lib.rs
@@ -1,5 +1,16 @@
 #![feature(box_syntax)]
 #![feature(duration_as_u128)]
+#![warn(
+    clippy::print_stdout,
+    clippy::unimplemented,
+    clippy::doc_markdown,
+    clippy::items_after_statements,
+    clippy::match_same_arms,
+    clippy::similar_names,
+    clippy::single_match_else,
+    clippy::use_self,
+    clippy::use_debug
+)]
 
 pub mod context;
 
